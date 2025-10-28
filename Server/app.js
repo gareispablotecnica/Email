79ac1935a3c71cd36e91f1637379f4c9693515f8
Pablo Gareis <gareispablotecnica1@gmail.com>
2025-10-28 14:34:49 -0300

    Envio de Emails

---
+++ Server/app.js
@@ -0,0 +1,15 @@
+const Express= require('express')
+const App= Express()
+
+require('dotenv').config()
+const PORT=process.env.PORT ||5000
+
+const cors= require('cors')
+App.use(cors())
+App.use(Express.json())
+
+
+
+App.listen(PORT, () => {
+    console.log(`🚀 http://localhost:${PORT}`)
+})
---
+++ Server/src/Utils/Email.js
@@ -0,0 +1,30 @@
+// ---> sirve para enviar correos electrónicos usando nodemailer y Gmail
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail", // o 'hotmail', 'outlook', 'yahoo'
+  auth: {
+    user: process.env.EMAIL_USER, // definido en .env
+    pass: process.env.EMAIL_PASS, // definido en .env
+  },
+});
+
+async function enviarCorreo(destinatario, asunto, cuerpo) {
+  try {
+    const info = await transporter.sendMail({
+      from: `"Servidor Node 👨‍💻" <${process.env.EMAIL_USER}>`,
+      to: destinatario,
+      subject: asunto,
+      text: cuerpo,
+      html: `<b>${cuerpo}</b>`,
+    });
+
+    console.log("Correo enviado:", info.messageId);
+    return { success: true, messageId: info.messageId };
+  } catch (error) {
+    console.error("Error al enviar el correo:", error);
+    return { success: false, error };
+  }
+}
+
+module.exports = enviarCorreo;
